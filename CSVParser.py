99f4e6a4d4865cda51a1de0db95d623f51e943db
timaeudg <timaeudg@rose-hulman.edu>
2013-10-07 21:34:14 -0400

    Added some changes to actually store the CSV data into a structure. Will need to alter how we do this a bit, as it seems to only leave about 300 megabytes to work with for the machine learning on my desktop, and since python uses memory based on the amount available for the machine, this could be a problem. Also included code for AVL tree that we may want to alter to use as a treemap. Also, DID NOT add the dataset to the repo as that may violate the use terms that we have for the competition, we should look into that. - Devon Timaeus

--- CSVParser.py
+++ CSVParser.py
@@ -1,13 +1,31 @@
 import sys
+import pyavltree
+from pyavltree import AVLTree
 
-fileToParse = "testCSV.txt"
+fileToParse = "tgmctrain.csv"
 # action = lambda dataLine: print
+csvData = AVLTree()
 
-def parseAndDoSomethingToLineData(fileToParse):
+def parseAndStoreLineData(fileToParse):
     with open(fileToParse) as inFile:
         for line in inFile:
-            seperatedLine = line.split(',')
-            print(seperatedLine)
+            separatedLine = line.split(',')
+            answerValues = []
+            for index in range(2, len(separatedLine)-1):
+                answerValues.append(float(separatedLine[index]))
+            questionKey = int(float(separatedLine[1]))
+            answerKey = int(float(separatedLine[0]))
+            if questionKey not in csvData:
+            #    The question is not in the dictionary, so we should add the question to the dictionary, and then the answer to the question's dictionary
+                csvData[questionKey] = []
+            print "Answer: ", answerKey
+            print "Questions: ", len(csvData.keys())
+            csvData[questionKey].append((answerKey, (answerValues, separatedLine[len(separatedLine)-1])))
+        numberOfQuestions = len(csvData.keys())
+        print "Number of Questions: ", numberOfQuestions
+        print "Question Numbers: "
+        for key in csvData:
+            print key
 
 
-parseAndDoSomethingToLineData(fileToParse)
+parseAndStoreLineData(fileToParse)
